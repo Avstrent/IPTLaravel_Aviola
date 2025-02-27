e31a3b9922ef874be4b43ba47a32bf72c4578a03
AviolaTrent <116270127+AviolaTrent@users.noreply.github.com>
2025-02-28 03:08:56 +0800

    Third Commit

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,26 @@
-<?php
+<?php 
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TeachersController;
+use App\Http\Controllers\ClassroomsController;
+use App\Http\Controllers\GradesController;
+use App\Http\Controllers\ParentsController;
+use App\Http\Controllers\StudentsController;
+use App\Http\Controllers\CoursesController;
+use App\Http\Controllers\ClassroomStudentsController;
+use App\Http\Controllers\Exam_TypesController;
+use App\Http\Controllers\ExamsController;
+use App\Http\Controllers\Exam_ResultsController;
+use App\Http\Controllers\AttendanceController;
 
-use App\Http\Controllers\UserController;
 
-// Route::get('/user', [UserController::class, 'show']);
-// Route::post('/user', [UserController::class, 'create']);
-// Route::put('/user', [UserController::class, 'update']);
-// Route::delete('/user', [UserController::class, 'delete']);
-Route::apiResource('/user', UserController::class);
-Route::resource('grades', GradeController::class);
-Route::resource('parents', ParentController::class);
-Route::resource('teachers', TeacherController::class);
-Route::resource('students', StudentController::class);
-Route::resource('courses', CourseController::class);
-Route::resource('classrooms', ClassroomController::class);
-Route::resource('classroom-students', ClassroomStudentController::class);
-Route::resource('attendance', AttendanceController::class);
-Route::resource('exam-types', ExamTypeController::class);
-Route::resource('exams', ExamController::class);
-Route::resource('exam-results', ExamResultController::class);
+Route::apiResource('teachers', TeachersController::class);
+Route::apiResource('classrooms', ClassroomsController::class);
+Route::apiResource('grades', GradesController::class);
+Route::apiResource('parents', ParentsController::class);
+Route::apiResource('students', StudentsController::class);
+Route::apiResource('courses', CoursesController::class);
+Route::apiResource('classroomStudents', ClassroomStudentsController::class);
+Route::apiResource('examtypes', Exam_TypesController::class);
+Route::apiResource('exam', ExamsController::class);
+Route::apiResource('examResult', Exam_ResultsController::class);
+Route::apiResource('examAttendance', AttendanceController::class);
